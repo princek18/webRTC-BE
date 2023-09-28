6613a26483f320e3c58d002a38dc16a6c8828e47
princek18 <princekumar181999@gmail.com>
2023-09-28 16:14:55 +0530

    cors fixed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://webrtc-be.onrender.com/",
+    origin: "*",
   },
 });
 const cors = require("cors");
